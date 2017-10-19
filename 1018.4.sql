74522ee2c7cf3a78b8958d4652b9cf1b4e35d48a
s16003 <s16003@std.it-college.ac.jp>
2017-10-19 14:02:03 +0900

    修正

--- 1018.4.sql
+++ 1018.4.sql
@@ -1,3 +1,5 @@
 select last_name, department_id, job_id
-from employees natural join locations
-where location_id = 1700;
+from employees 
+where department_id in (select department_id 
+										from departments
+										where location_id = 1700);
