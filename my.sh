241fdbbf83ade7c3fd47ae2216156a60decc66b0
s16003 <s16003@std.it-college.ac.jp>
2017-11-15 16:14:07 +0900

    first commit

--- my.sh
+++ my.sh
@@ -7,6 +7,9 @@ PORT=1521
 SID=orcl
 BASE=${HOME}/instantclient_12_1
 
+NLS_LANG=JAPANESE_JAPAN.AL32UTF8
+export NLS_LANG
+
 export LD_LIBRARY_PATH=${BASE}:${LD_LIBRARY_PATH}
 export SQLPATH=${BASE}:${SQL_PATH}
 export no_proxy="127.0.0.1,localhost,$ADDR"
